061adf3ed7b3992f14a8b31506448079afefe8b5
Luis Ruiz <Luis Ruiz@LAPTOP-MUKR0LNR>
2019-11-03 20:54:05 +0000

    MoneyCalculator versión 2

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,6 +1,6 @@
 package moneycalculator;
 
-    import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -9,13 +9,33 @@ import java.util.Scanner;
 
 public class MoneyCalculator {
 
-public static void main(String[] args) throws IOException {
-        System.out.println("Introduce una cantidad de dólares: ");
+    public static void main(String[] args) throws IOException {
+        MoneyCalculator moneyCalculator = new MoneyCalculator();
+        moneyCalculator.control();
+    }
+    
+    private double amount;
+    private double exchangeRate;
+    
+    private void control() throws IOException{
+        input();
+        process();
+        output();
+    }
+    
+    
+    private void input() {
+        System.out.println("Introduzca una cantidad en dólares: ");
         Scanner scanner = new Scanner(System.in);
-        double amount = scanner.nextDouble();
-        double exchangerate = getExchangeRate("USD","EUR");
-        System.out.println(amount + " dolares equivalen a " 
-                + amount*exchangerate + " euros");        
+        amount = Double.parseDouble(scanner.next());
+    }
+    
+    private void process() throws IOException {
+        exchangeRate = getExchangeRate("USD","EUR");
+    }
+    
+    private void output() {
+        System.out.println(amount + " USD equivalen a " + amount*exchangeRate + " EUR");
     }
     
     private static double getExchangeRate(String from, String to) throws IOException {
@@ -30,5 +50,5 @@ public static void main(String[] args) throws IOException {
             String line1 = line.substring(line.indexOf(to)+12, line.indexOf("}"));
             return Double.parseDouble(line1);
         }
-    }      
+    }            
 }
